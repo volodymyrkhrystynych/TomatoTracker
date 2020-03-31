9b89e38602fe373b4668c3f4e9dcf9152afaf471
Volodymyr Khrystynych <v.khryst@outlook.com>
2020-03-31 19:28:01 -0400

    added a bit of styling

--- src/timer/countdown.js
+++ src/timer/countdown.js
@@ -1,50 +1,61 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import { Grid, Paper, Container } from "@material-ui/core";
 
 const Countdown = () => {
 	const [seconds, setSeconds] = React.useState(0);
 	const [counter, setCounter] = React.useState(null);
 
 	const startCountdown = () => {
-        if (counter == null){
-            let interval = setInterval(countUp, 1000);
-            setCounter(interval);
-        } else {
-            clearInterval(counter);
-            setCounter(null);
-        };
-    };
-    
-    const resetCountdown = () => {
-        setSeconds(0);
-    };
+		if (counter == null) {
+			let interval = setInterval(countUp, 1000);
+			setCounter(interval);
+		} else {
+			clearInterval(counter);
+			setCounter(null);
+		}
+	};
+
+	const resetCountdown = () => {
+		setSeconds(0);
+	};
 
 	const secondsToTime = secs => {
-		let hours = "00" +  Math.floor(secs / (60 * 60));
+		let hours = "00" + Math.floor(secs / (60 * 60));
 
 		let divisor_for_minutes = secs % (60 * 60);
-		let minutes = "00" +  Math.floor(divisor_for_minutes / 60);
+		let minutes = "00" + Math.floor(divisor_for_minutes / 60);
 
 		let divisor_for_seconds = divisor_for_minutes % 60;
-		let seconds = "00" +  Math.ceil(divisor_for_seconds);
+		let seconds = "00" + Math.ceil(divisor_for_seconds);
 
 		let obj = {
 			h: hours.slice(-2),
 			m: minutes.slice(-2),
 			s: seconds.slice(-2)
-        };
-        
-		return obj.h + ":" + obj.m + ":" + obj.s
+		};
+
+		return obj.h + ":" + obj.m + ":" + obj.s;
 	};
 	const countUp = () => {
 		setSeconds(seconds => seconds + 1);
 	};
 
 	return (
-		<>
-			<Button onClick={startCountdown}>{secondsToTime(seconds)}</Button>
-			<Button onClick={resetCountdown}>Reset</Button>
-		</>
+		<Grid container direction="column" alignItems="center">
+			<Paper style={{ width: 100 }}>
+				<Grid item xs={12}>
+					<Button onClick={startCountdown}>
+						{secondsToTime(seconds)}
+					</Button>
+				</Grid>
+				<Grid item xs={12}>
+					<Button onClick={resetCountdown}>
+						Reset
+					</Button>
+				</Grid>
+			</Paper>
+		</Grid>
 	);
 };
 
