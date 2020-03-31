b77d074cbdd772d44380106487392762963d6123
Volodymyr <v.khryst@outlook.com>
2020-03-30 20:28:01 -0400

    counter now has a reset button and can start and stop

--- src/timer/countdown.js
+++ src/timer/countdown.js
@@ -1,42 +1,49 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import SelectInput from "@material-ui/core/Select/SelectInput";
 
 const Countdown = () => {
 	const [seconds, setSeconds] = React.useState(0);
+	const [counter, setCounter] = React.useState(null);
 
 	const startCountdown = () => {
-		setInterval(countUp, 1000);
-	};
+        if (counter == null){
+            let interval = setInterval(countUp, 1000);
+            setCounter(interval);
+        } else {
+            clearInterval(counter);
+            setCounter(null);
+        };
+    };
+    
+    const resetCountdown = () => {
+        setSeconds(0);
+    };
 
 	const secondsToTime = secs => {
-		let hours = Math.floor(secs / (60 * 60));
+		let hours = "00" +  Math.floor(secs / (60 * 60));
 
 		let divisor_for_minutes = secs % (60 * 60);
-		let minutes = Math.floor(divisor_for_minutes / 60);
+		let minutes = "00" +  Math.floor(divisor_for_minutes / 60);
 
 		let divisor_for_seconds = divisor_for_minutes % 60;
-		let seconds = Math.ceil(divisor_for_seconds);
+		let seconds = "00" +  Math.ceil(divisor_for_seconds);
 
 		let obj = {
-			h: hours,
-			m: minutes,
-			s: seconds
-		};
-		console.log(obj);
-		return minutes + ":" + seconds
+			h: hours.slice(-2),
+			m: minutes.slice(-2),
+			s: seconds.slice(-2)
+        };
+        
+		return obj.h + ":" + obj.m + ":" + obj.s
 	};
 	const countUp = () => {
 		setSeconds(seconds => seconds + 1);
 	};
 
-	//setInterval(countUp, 1000);
-
 	return (
 		<>
-			<label>{secondsToTime(seconds)}</label>
-			<Button onClick={startCountdown}>Start</Button>
-
+			<Button onClick={startCountdown}>{secondsToTime(seconds)}</Button>
+			<Button onClick={resetCountdown}>Reset</Button>
 		</>
 	);
 };
